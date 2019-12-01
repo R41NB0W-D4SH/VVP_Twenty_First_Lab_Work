0c52c1e1f61818c2a9a49e5a705ac5e44a1dc337
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 18:17:10 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int k = 1;
+	string line;
+
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	for (int i = 0; i < line.length() - 1; i++)
+	{
+		if (line[i] == ' ' & line[i + 1] != ' ') k++;
+	}
+
+	cout << "Кол-во слов в строке: " << k << endl;
+
+	system("pause");
+	return 0;
+}
