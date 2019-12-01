5b263349788bda5d7dae9f36631e1f846e3c82ab
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 18:36:22 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string line;
+
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	cout << endl << line[0];
+	for (int i = 1; i < line.length(); i++)
+	{
+		if (line[i] == line[0])
+		{
+			cout << ".";
+		}
+		else
+		{
+			cout << line[i];
+		}
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
