499c356e068a019c8fb81644a0b27e4c921f8af9
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 18:25:26 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int min = 256, k = 0;
+	string line;
+
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	for (int i = 0; i < line.length(); i++)
+	{
+		if (line[i] != ' ')
+		{
+			k++;
+		}
+		else
+		{
+			if (k < min) min = k;
+			k = 0;
+		}
+	}
+
+	cout << "Длина самого короткого слова в строке: " << k << endl;
+
+	system("pause");
+	return 0;
+}
