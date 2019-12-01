093db2f6781dc36d57e6ba2ff8fee4c70363dbc8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 20:40:59 +0300

    Update task6.cpp

--- task6.cpp
+++ task6.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string line;
+	int i;
+
+	cout << "Введите полный путь файла: ";
+	getline(cin, line);
+
+	i = line.length() - 1;
+
+	while (line[i] != '\\')
+	{
+		i--;
+	}
+	i -= 2;
+
+	while (line[i] != '\\')
+	{
+		if (i != 0) i--;
+		else
+		{
+			cout << "\\";
+			return 0;
+		}
+	}
+
+	i++;
+	cout << "Название последнего каталога: ";
+	while (line[i] != '\\') cout << line[i++];
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
