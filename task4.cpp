d6e46a995b4bb245325d94646df7b640159d5c59
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 19:00:29 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int k = 0;
+	string line;
+
+	cout << "Введите строку: ";
+	getline(cin, line);
+
+	for (int i = 0; i < line.length(); i++)
+	{
+		if (line[i] == 'а' || line[i] == 'А' || line[i] == 'у' || line[i] == 'У' || line[i] == 'е' || line[i] == 'Е' || line[i] == 'ы' || line[i] == 'Ы' || line[i] == 'о' || line[i] == 'О' || line[i] == 'э' || line[i] == 'Э' || line[i] == 'я' || line[i] == 'Я' || line[i] == 'и' || line[i] == 'И' || line[i] == 'ю' || line[i] == 'Ю' || line[i] == 'ё' || line[i] == 'Ё') 
+		{
+			k++;
+		}
+	}
+	cout << "Кол-во гласных букв в строке: " << k << endl;
+
+	system("pause");
+	return 0;
+}
