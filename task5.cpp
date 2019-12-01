bd24f9129caa66624fa811d1d5c328869273e856
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 19:09:50 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string line;
+	int i, j = 0;
+
+	cout << "Введите полный путь до нужного файла: ";
+	getline(cin, line);
+
+	for (i = line.length() - 1; i > 0; i--)
+	{
+		if (line[i] == '.')
+		{
+			i--;
+			while (line[i] != '\\')
+			{
+				i--;
+			}
+			i++;
+			cout << "Имя файла: ";
+			while (line[i] != '.')
+			{
+				cout << line[i];
+				i++;
+			}
+			break;
+		}
+	}
+	cout << endl;
+  
+	system("pause");
+	return 0;
+}
